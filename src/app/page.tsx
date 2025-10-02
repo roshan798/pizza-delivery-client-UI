bfe48c85bb2b37c612c4bb980968144f0406af5c
Roshan Kumar <rsahu7989@gmail.com>
2025-10-02 20:27:19 +0530

    initialized client ui

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="">
+      <h1>Welcome to Pizza App</h1>
+      <Button className="cursor-pointer">Shadcn Button</Button>
+    </div>
+  );
+}
